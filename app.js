2f458205a36df0771db9324675494a404a14713d
Gintare Venckiene <gintare.venckiene@gmail.com>
2020-12-20 21:00:13 +0200

    Created express.js server sample

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const http = require('http');
-const routes = require('./routes')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(3001);
+app.use(bodyParser.urlencoded());
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="Title" /><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from Express</h1>');
+});
+
+app.listen(3001)
