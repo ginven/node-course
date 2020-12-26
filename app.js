8744f18181dcaec0352b60d8e6adf30b87a50893
Gintare Venckiene <gintare.venckiene@gmail.com>
2020-12-26 22:26:03 +0200

    Deleted all the unecessary templating files.

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -10,19 +9,8 @@ const rootDir = require('./util/path')
 
 const app = express();
 
-// EJS
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
-
-// HANDLEBARS
-// app.engine('hbs', expressHbs({layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs'}));
-// app.set('view engine', 'hbs');
-// app.set('views', 'views');
-
-// PUG
-// app.set('view engine', 'pug');
-// app.set('views', 'views');
 
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(rootDir, 'public')));
