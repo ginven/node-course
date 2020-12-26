57b4dc26fd032335737e1cd3c8a947bbf5595155
Gintare Venckiene <gintare.venckiene@gmail.com>
2020-12-26 22:22:51 +0200

    Learning some templates: pug, handlebars and ejs

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+// const expressHbs = require('express-handlebars');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const rootDir = require('./util/path')
 
 const app = express();
 
+// EJS
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+
+// HANDLEBARS
+// app.engine('hbs', expressHbs({layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs'}));
+// app.set('view engine', 'hbs');
+// app.set('views', 'views');
+
+// PUG
+// app.set('view engine', 'pug');
+// app.set('views', 'views');
+
 app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="Title" /><button type="submit">Add Product</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
 app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express</h1>');
-});
-
+    res.status(404).render('not-found', {docTitle: '404 - Page not found!'});
+})
 app.listen(3001)
